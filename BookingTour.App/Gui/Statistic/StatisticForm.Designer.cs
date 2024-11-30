e6b7deb89a26236d8de3fda6bf92bdd4b0b0abe0
SangNguyen <nngocsang38@gmail.com>
2024-11-30 11:37:29 +0700

    last update 2

---
+++ BookingTour.App/Gui/Statistic/StatisticForm.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace BookingTour.App.Gui.Statistic
+{
+    partial class StatisticForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            dataGridView1 = new DataGridView();
+            panel3 = new Panel();
+            yearSelect3 = new ComboBox();
+            tabPage2 = new TabPage();
+            panel2 = new Panel();
+            yearSelect2 = new ComboBox();
+            cartesianChart2 = new LiveChartsCore.SkiaSharpView.WinForms.CartesianChart();
+            tabPage3 = new TabPage();
+            panel1 = new Panel();
+            yearSelect = new ComboBox();
+            cartesianChart1 = new LiveChartsCore.SkiaSharpView.WinForms.CartesianChart();
+            tabControl1.SuspendLayout();
+            tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            panel3.SuspendLayout();
+            tabPage2.SuspendLayout();
+            panel2.SuspendLayout();
+            tabPage3.SuspendLayout();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Controls.Add(tabPage3);
+            tabControl1.Dock = DockStyle.Fill;
+            tabControl1.Location = new Point(0, 0);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(998, 617);
+            tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            tabPage1.BackColor = Color.FromArgb(244, 243, 247);
+            tabPage1.Controls.Add(dataGridView1);
+            tabPage1.Controls.Add(panel3);
+            tabPage1.Location = new Point(4, 24);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(990, 589);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "Chuyến đi";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Dock = DockStyle.Bottom;
+            dataGridView1.Location = new Point(3, 41);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(984, 545);
+            dataGridView1.TabIndex = 4;
+            // 
+            // panel3
+            // 
+            panel3.Controls.Add(yearSelect3);
+            panel3.Dock = DockStyle.Top;
+            panel3.Location = new Point(3, 3);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(984, 32);
+            panel3.TabIndex = 3;
+            // 
+            // yearSelect3
+            // 
+            yearSelect3.Dock = DockStyle.Right;
+            yearSelect3.FormattingEnabled = true;
+            yearSelect3.Location = new Point(766, 0);
+            yearSelect3.Name = "yearSelect3";
+            yearSelect3.Size = new Size(218, 23);
+            yearSelect3.TabIndex = 0;
+            yearSelect3.SelectedIndexChanged += yearSelect3_SelectedIndexChanged;
+            // 
+            // tabPage2
+            // 
+            tabPage2.Controls.Add(panel2);
+            tabPage2.Controls.Add(cartesianChart2);
+            tabPage2.Location = new Point(4, 24);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(990, 589);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "Lịch trình";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(yearSelect2);
+            panel2.Dock = DockStyle.Top;
+            panel2.Location = new Point(3, 3);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(984, 32);
+            panel2.TabIndex = 2;
+            // 
+            // yearSelect2
+            // 
+            yearSelect2.Dock = DockStyle.Right;
+            yearSelect2.FormattingEnabled = true;
+            yearSelect2.Location = new Point(766, 0);
+            yearSelect2.Name = "yearSelect2";
+            yearSelect2.Size = new Size(218, 23);
+            yearSelect2.TabIndex = 0;
+            yearSelect2.SelectedIndexChanged += yearSelect2_SelectedIndexChanged;
+            // 
+            // cartesianChart2
+            // 
+            cartesianChart2.Dock = DockStyle.Bottom;
+            cartesianChart2.Location = new Point(3, 56);
+            cartesianChart2.Name = "cartesianChart2";
+            cartesianChart2.Size = new Size(984, 530);
+            cartesianChart2.TabIndex = 1;
+            // 
+            // tabPage3
+            // 
+            tabPage3.Controls.Add(panel1);
+            tabPage3.Controls.Add(cartesianChart1);
+            tabPage3.Location = new Point(4, 24);
+            tabPage3.Name = "tabPage3";
+            tabPage3.Padding = new Padding(3);
+            tabPage3.Size = new Size(990, 589);
+            tabPage3.TabIndex = 2;
+            tabPage3.Text = "Tổng doanh thu";
+            tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(yearSelect);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(3, 3);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(984, 32);
+            panel1.TabIndex = 1;
+            // 
+            // yearSelect
+            // 
+            yearSelect.Dock = DockStyle.Right;
+            yearSelect.FormattingEnabled = true;
+            yearSelect.Location = new Point(766, 0);
+            yearSelect.Name = "yearSelect";
+            yearSelect.Size = new Size(218, 23);
+            yearSelect.TabIndex = 0;
+            yearSelect.SelectedIndexChanged += YearSelect_SelectedIndexChanged;
+            // 
+            // cartesianChart1
+            // 
+            cartesianChart1.Dock = DockStyle.Bottom;
+            cartesianChart1.Location = new Point(3, 56);
+            cartesianChart1.Name = "cartesianChart1";
+            cartesianChart1.Size = new Size(984, 530);
+            cartesianChart1.TabIndex = 0;
+            // 
+            // StatisticForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(998, 617);
+            Controls.Add(tabControl1);
+            Name = "StatisticForm";
+            Text = "StatisticForm";
+            Load += StatisticForm_Load;
+            tabControl1.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            panel3.ResumeLayout(false);
+            tabPage2.ResumeLayout(false);
+            panel2.ResumeLayout(false);
+            tabPage3.ResumeLayout(false);
+            panel1.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
+        private TabPage tabPage3;
+        private Panel panel1;
+        private LiveChartsCore.SkiaSharpView.WinForms.CartesianChart cartesianChart1;
+        private ComboBox yearSelect;
+        private LiveChartsCore.SkiaSharpView.WinForms.CartesianChart cartesianChart2;
+        private Panel panel2;
+        private ComboBox yearSelect2;
+        private Panel panel3;
+        private ComboBox yearSelect3;
+        private DataGridView dataGridView1;
+    }
+}
---
+++ BookingTour.App/Gui/Statistic/StatisticForm.cs
@@ -0,0 +1,277 @@
+﻿using BookingTour.App.Bus;
+using BookingTour.App.Models;
+using LiveChartsCore;
+using LiveChartsCore.Measure;
+using LiveChartsCore.SkiaSharpView;
+
+namespace BookingTour.App.Gui.Statistic;
+
+public partial class StatisticForm : Form
+{
+    public StatisticForm()
+    {
+        InitializeComponent();
+    }
+
+    private void StatisticForm_Load(object sender, EventArgs e)
+    {
+        LoadTimeChart();
+        LoadItineraryChart();
+        LoadTourTable();
+    }
+
+    private void LoadTimeChart()
+    {
+        var currentYear = DateTime.Now.Year;
+        for (var year = currentYear - 10; year <= currentYear; year++)
+        {
+            yearSelect.Items.Add(year);
+        }
+        yearSelect.SelectedIndex = 10;
+        var startDate = GetFirstDayOfYear(currentYear);
+        var endDate = GetLastDayOfYear(currentYear);
+        var statisticsData = StatisticsBus.Instance.GetStatisticsByTime(startDate, endDate);
+        UpdateRevenueChart(statisticsData ?? [new StatisticsByTime
+            {
+                Period = "",
+                TotalRevenue = 0,
+                TotalTours = 0,
+                TotalTickets = 0
+            }
+        ]);
+    }
+
+    private void LoadItineraryChart()
+    {
+        var currentYear = DateTime.Now.Year;
+        for (var year = currentYear - 10; year <= currentYear; year++)
+        {
+            yearSelect2.Items.Add(year);
+        }
+        yearSelect2.SelectedIndex = 10;
+        var startDate = GetFirstDayOfYear(currentYear);
+        var endDate = GetLastDayOfYear(currentYear);
+        var statisticsData = StatisticsBus.Instance.GetStatisticsByItinerary(startDate, endDate);
+        UpdateItineraryChart(statisticsData ?? [new StatisticsByItinerary
+            {
+                ItineraryId = 0,
+                ItineraryName = "",
+                TotalRevenue = 0,
+                TotalTours = 0
+            }
+        ]);
+    }
+
+    public void UpdateRevenueChart(List<StatisticsByTime> data)
+    {
+        // Cấu hình Labels trục X
+        var labels = data.Select(d => d.Period).ToArray();
+
+        // Series 1: Doanh thu
+        var revenueSeries = new ColumnSeries<decimal>
+        {
+            Values = data.Select(d => d.TotalRevenue).ToArray(),
+            Name = "Doanh thu (Revenue)"
+        };
+
+        // Series 2: Số tour
+        var toursSeries = new LineSeries<int>
+        {
+            Values = data.Select(d => d.TotalTours).ToArray(),
+            Name = "Số tour (Tours)"
+        };
+
+        // Series 3: Số vé
+        var ticketsSeries = new LineSeries<int>
+        {
+            Values = data.Select(d => d.TotalTickets).ToArray(),
+            Name = "Số vé (Tickets)"
+        };
+
+        // Cập nhật Chart
+        cartesianChart1.Series = new ISeries[] { revenueSeries, toursSeries, ticketsSeries };
+
+        // Gán trục X
+        cartesianChart1.XAxes = new[]
+        {
+            new Axis
+            {
+                Labels = labels,
+                LabelsRotation = 15 // Xoay nhãn nếu cần
+            }
+        };
+
+        // Gán trục Y
+        cartesianChart1.YAxes = new[]
+        {
+            new Axis
+            {
+                Name = "Giá trị"
+            }
+        };
+
+        // Cấu hình Tooltip
+        cartesianChart1.TooltipPosition = TooltipPosition.Top; // Hiển thị tooltip ở trên
+        cartesianChart1.TooltipTextSize = 14;
+    }
+
+    public void UpdateItineraryChart(List<StatisticsByItinerary> data)
+    {
+        var xLabels = data.Select(item => item.ItineraryName).ToArray();
+
+        var totalRevenueSeries = new ColumnSeries<decimal>
+        {
+            Name = "Total Revenue",
+            Values = data.Select(item => item.TotalRevenue).ToArray(),
+        };
+
+        var totalToursSeries = new ColumnSeries<int>
+        {
+            Name = "Total Tours",
+            Values = data.Select(item => item.TotalTours).ToArray(),
+        };
+
+        cartesianChart2.XAxes = new Axis[]
+        {
+            new Axis
+            {
+                Labels = xLabels,
+                Name = "Itineraries",
+                LabelsRotation = 15 // Góc xoay nhãn trục X nếu quá dài
+            }
+        };
+
+        cartesianChart2.YAxes = new Axis[]
+        {
+            new Axis
+            {
+                Name = "Values",
+                NamePaint = new LiveChartsCore.SkiaSharpView.Painting.SolidColorPaint
+                {
+                    Color = SkiaSharp.SKColors.Black
+                }
+            }
+        };
+        cartesianChart2.Series = new ISeries[] { totalRevenueSeries, totalToursSeries };
+    }
+
+    private void LoadTourTable()
+    {
+        var currentYear = DateTime.Now.Year;
+        for (var year = currentYear - 10; year <= currentYear; year++)
+        {
+            yearSelect3.Items.Add(year);
+        }
+        yearSelect3.SelectedIndex = 10;
+        var startDate = GetFirstDayOfYear(currentYear);
+        var endDate = GetLastDayOfYear(currentYear);
+        var statisticsData = StatisticsBus.Instance.GetStatisticsByTour(startDate, endDate);
+        UpdateTourStatistic(statisticsData ?? [new StatisticsByTour
+            {
+                TourId = 0,
+                DateStart = default,
+                DateEnd = default,
+                TotalRevenue = 0,
+                Capacity = 0,
+                BookedSlots = 0,
+                FillRate = 0
+            }
+        ]);
+    }
+
+    private void UpdateTourStatistic(List<StatisticsByTour> data)
+    {
+        // Xóa tất cả cột và dòng hiện có trong DataGridView
+        dataGridView1.Columns.Clear();
+        dataGridView1.Rows.Clear();
+
+        // Thêm các cột vào DataGridView
+        dataGridView1.Columns.Add("TourId", "Tour ID");
+        dataGridView1.Columns.Add("DateStart", "Start Date");
+        dataGridView1.Columns.Add("DateEnd", "End Date");
+        dataGridView1.Columns.Add("TotalRevenue", "Total Revenue");
+        dataGridView1.Columns.Add("Capacity", "Capacity");
+        dataGridView1.Columns.Add("BookedSlots", "Booked Slots");
+        dataGridView1.Columns.Add("FillRate", "Fill Rate (%)");
+
+        // Định dạng hiển thị cho các cột ngày và tiền tệ
+        dataGridView1.Columns["DateStart"].DefaultCellStyle.Format = "dd/MM/yyyy";
+        dataGridView1.Columns["DateEnd"].DefaultCellStyle.Format = "dd/MM/yyyy";
+        dataGridView1.Columns["TotalRevenue"].DefaultCellStyle.Format = "C"; // Hiển thị theo định dạng tiền tệ
+        dataGridView1.Columns["FillRate"].DefaultCellStyle.Format = "P1";   // Hiển thị theo định dạng phần trăm
+
+        // Đổ dữ liệu từ danh sách vào DataGridView
+        foreach (var item in data)
+        {
+            dataGridView1.Rows.Add(
+                item.TourId,
+                item.DateStart,
+                item.DateEnd,
+                item.TotalRevenue,
+                item.Capacity,
+                item.BookedSlots,
+                item.FillRate / 100 // Chuyển đổi từ tỷ lệ phần trăm sang định dạng số
+            );
+        }
+
+        // Tự động điều chỉnh kích thước cột
+        dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+    }
+
+    public DateOnly GetFirstDayOfYear(int year)
+    {
+        // Ngày đầu tiên của năm
+        return new DateOnly(year, 1, 1);
+    }
+
+    public DateOnly GetLastDayOfYear(int year)
+    {
+        // Ngày cuối cùng của năm (31 tháng 12)
+        return new DateOnly(year, 12, 31);
+    }
+
+    private void YearSelect_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        if (yearSelect.SelectedItem == null) return;
+        var selectedYear = (int)yearSelect.SelectedItem;
+        LoadStatisticsRevuenueForYear(selectedYear);
+    }
+
+    private void LoadStatisticsRevuenueForYear(int year)
+    {
+        var startDate = GetFirstDayOfYear(year);
+        var endDate = GetLastDayOfYear(year);
+        var statisticsData = StatisticsBus.Instance.GetStatisticsByTime(startDate, endDate);
+        if (statisticsData != null) UpdateRevenueChart(statisticsData);
+    }
+
+    private void yearSelect2_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        if (yearSelect2.SelectedItem == null) return;
+        var selectedYear = (int)yearSelect2.SelectedItem;
+        LoadStatisticsItineraryForYear(selectedYear);
+    }
+
+    private void LoadStatisticsItineraryForYear(int year)
+    {
+        var startDate = GetFirstDayOfYear(year);
+        var endDate = GetLastDayOfYear(year);
+        var statisticsData = StatisticsBus.Instance.GetStatisticsByItinerary(startDate, endDate);
+        if (statisticsData != null) UpdateItineraryChart(statisticsData);
+    }
+
+    private void yearSelect3_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        if (yearSelect3.SelectedItem == null) return;
+        var selectedYear = (int)yearSelect3.SelectedItem;
+        LoadStatisticcTourForYear(selectedYear);
+    }
+
+    private void LoadStatisticcTourForYear(int year)
+    {
+        var startDate = GetFirstDayOfYear(year);
+        var endDate = GetLastDayOfYear(year);
+        var statisticsData = StatisticsBus.Instance.GetStatisticsByTour(startDate, endDate);
+        if (statisticsData != null) UpdateTourStatistic(statisticsData);
+    }
+}
